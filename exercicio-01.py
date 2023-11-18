569d7ba13d18de0f181a6a0a1c4fdc06500af7e4
aisha <mattosaisha@gmail.com>
2023-11-17 21:30:56 -0300

    Mudar atividade

---
+++ exercicio-01.py
@@ -0,0 +1,30 @@
+nome = "Helena" #Uma string
+idade = 31 #Um inteiro
+altura = 1.79 #Ponto flutuante
+estudante = False #Boolean
+
+print(nome)
+print(idade)
+print(altura)
+print(estudante)
+
+
+#operador matemático
+igual = 5==3 #igual: false
+diferente = 5 !=3 #diferente: True
+maior = 5 > 3 # maior : True
+menor = 5 < 3 # menor: False
+
+#operadores lógicos
+e = True and False # e : False
+ou = True or False # ou: True
+nao = not True #nao: False
+
+# math
+
+valor1 = 10
+valor2 = 30
+soma = valor1 + valor2
+diferenca = valor1 - valor2
+
+print("Some: ", soma, "Diferença:", diferenca)
