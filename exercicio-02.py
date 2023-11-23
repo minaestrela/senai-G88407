332c7242f6c6555856a8d74b4122f372de53128c
aisha <mattosaisha@gmail.com>
2023-11-22 21:21:52 -0300

    Criar boolean

---
+++ exercicio-02.py
@@ -0,0 +1,18 @@
+idade = 15
+
+if idade >= 18:
+    print("Você é maior de idade!", idade)
+else:
+    print("Você não é maior de idade!", idade)
+    
+altura = 170
+
+if altura >= 170:
+    alto = True
+elif altura < 170:
+    alto = False
+    
+if alto:
+    print("Arrasor veyr!")
+else:
+    print("Micor meo...")
