0aaec80cb36a43c1d0e90e9b2ea9d809013e5131
aisha <mattosaisha@gmail.com>
2023-12-15 20:43:09 -0300

    Atualizar avaliacao

--- avaliacao-01.py
+++ avaliacao-01.py
@@ -4,46 +4,50 @@ conta = {
 }
 
 def auntenticar_conta():
-    senha = input("Insira sua senha.")
+    senha = input("Insira sua senha. ")
     if senha in conta:
-        print("Sua senha é válida.")
+        print("Sua senha é válida. ")
+        return senha
     else:
-        print("Senha inválida.")
+        print("Senha inválida. ")
 
-def verificar_saldo():
-    saldo = conta[saldo]
+def verificar_saldo(senha_local):
+    print (conta[senha_local])
     
-def depositar_valor():
-    deposito = input("Insira o valor de  depósito.")
-    if deposito in conta:
-        conta[deposito] = conta[deposito] + deposito
-def retirar_valor():
-    saque = input("Insira o valor de saque.")
-    if saque in conta:
-        if saque < conta[saque]:
-            print("Valor indisponível para saque.")
+    
+def depositar_valor(senha_local):
+    deposito = int(input("Insira o valor de  depósito. "))
+    conta[senha_local] = conta[senha_local] + deposito
+
+def retirar_valor(senha_local):
+    saque = int(input("Insira o valor de saque. "))
+    if saque > conta[senha_local]:
+        print("Valor indisponível para saque. ")
     else:
-        saque = saque + conta[saque]
+        conta[senha_local] = conta[senha_local] - saque
         
-def operaçoes_bancarias():
+def operaçoes_bancarias(senha_local):
 
     while True:
  
         ask = int(input("Insira uma das opções.\n"
-                "1: Verificar saldo\n"
-                "2: Depositar dinheiro\n"
-                "3: Retirar dinheiro\n"
-                "4: Sair\n"
+                "[ 1 ]: Verificar saldo\n"
+                "[ 2 ]: Depositar dinheiro\n"
+                "[ 3 ]: Retirar dinheiro\n"
+                "[ 4 ]: Sair\n"
                 ": ")) 
         
         if ask == 1:
-            print(verificar_saldo)
+            verificar_saldo(senha_local)
         elif ask == 2:
-            depositar_valor()
+            depositar_valor(senha_local)
         elif ask == 3:
-            retirar_valor()
+            retirar_valor(senha_local)
         elif ask == 4:
-            print("Encerramos por aqui!")
+            print("Encerramos por aqui! ")
             break
         else:
-            print("Opção inválida. Tente novamente.")
+            print("Opção inválida. Tente novamente. ")
+        
+senha = auntenticar_conta()
+operaçoes_bancarias(senha)
